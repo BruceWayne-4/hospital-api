81982338f63340a68b06eb99c5e0413b6823e5b9
Ricky <1308543433@qq.com>
2017-05-23 22:17:34 +0800

    修改内容

---
+++ app/models/user.rb
@@ -0,0 +1,13 @@
+class User < ApplicationRecord
+  before_create { generate_token(:auth_token) }
+
+  protected
+
+  # 在每次生成admin时添加auth_token
+  def generate_token(column)
+    begin
+      self[column] = SecureRandom.urlsafe_base64
+    end while User.exists?(column => self[column])
+  end
+end
+
---
+++ db/migrate/20170523141523_create_users.rb
@@ -0,0 +1,15 @@
+class CreateUsers < ActiveRecord::Migration[5.1]
+  def change
+    create_table :users do |t|
+      t.string :name
+      t.string :phone
+      t.string :gender
+      t.string :id_card_no
+      t.string :password_digest
+      t.string :auth_token
+      t.integer :card_id
+
+      t.timestamps
+    end
+  end
+end
